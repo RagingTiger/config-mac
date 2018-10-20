0fa096d71a65ba69bdecba8067df2fac50278974
01101010 <jander43@vols.utk.edu>
2018-10-20 12:44:49 -0400

    Updated with improved readability

--- config-mac.sh
+++ config-mac.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+# globals
+BREW_URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
+CLI_DEP="https://raw.githubusercontent.com/RagingTiger/config-mac/master/config/brew_cli.txt"
+
 # funcs
 get_brew(){
   # install brew first
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL ${BREW_URL})"
 }
 
 ex_brew(){
   # get cli dependencies for install
-  brew install "$(cat config/brew_cli.txt | tr '\n' ' ')"
+  brew install "$(curl -fsSL ${CLI_DEP} | tr '\n' ' ')"
 }
+
+main(){
+  # first download brew
+  get_brew
+
+  # next install dependencies
+  ex_brew
+}
+
+# executable
+main
