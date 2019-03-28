3f38c4394357b62660d0e46f5cfbeddb6279740a
John Anderson <tigerj@Johns-MacBook-Pro.local>
2019-03-28 13:38:40 -0400

    Updated with GUI dependencies

--- config-mac.sh
+++ config-mac.sh
@@ -3,6 +3,7 @@
 # globals
 BREW_URL="https://raw.githubusercontent.com/Homebrew/install/master/install"
 CLI_DEP="https://raw.githubusercontent.com/RagingTiger/config-mac/master/config/brew_cli.txt"
+GUI_DEP="https://raw.githubusercontent.com/RagingTiger/config-mac/master/config/brew_gui.txt"
 
 # funcs
 get_brew(){
@@ -13,6 +14,9 @@ get_brew(){
 ex_brew(){
   # get cli dependencies for install
   brew install $(curl -fsSL ${CLI_DEP} | tr '\n' ' ')
+
+  # get gui depedencies for install
+  brew cask install $(curl -fsSL ${GUI_DEP} | tr '\n' ' ')
 }
 
 main(){
